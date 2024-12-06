f02d9913098802e4d951d9cf3495bfe62a1114ea
Philgyam <philemongyamfi9@gmail.com>
2024-12-06 08:21:22 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 
-import ChatInterface from './ChatPage'; // Adjust the import path if needed
+import ChatInterface from './chatPage'; // Adjust the import path if needed
 import Homepage from './HomePage'; // Adjust the import path if needed
 
 function App() {
